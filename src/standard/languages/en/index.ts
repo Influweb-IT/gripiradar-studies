89ea5a67c46cdda66e95d5d11812cc9e754278bf
Matteo Delfino <matteo.delfino@isi.it>
2024-11-28 16:30:34 +0100

    intake: add stubs for Q5 help text

--- src/standard/languages/en/index.ts
+++ src/standard/languages/en/index.ts
@@ -1,9 +1,11 @@
 import { LanguageHelpers } from "../../../common";
 
+import intake from "./intake.json";
 import vaccination from "./vaccination.json";
 import weekly from "./weekly.json";
 
 const languageId = "en";
 
+LanguageHelpers.addLanguage(languageId, intake);
 LanguageHelpers.addLanguage(languageId, vaccination);
 LanguageHelpers.addLanguage(languageId, weekly);
